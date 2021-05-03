d0e0aab74b65aa86a181350e7c0910b9b89cabf8
ReAlign <realign@yeah.net>
2021-05-03 11:40:10 +0800

    re:remove old

--- scripts/get-real-name.js
+++
@@ -1,8 +0,0 @@
-const getRealName = (str) => {
-    const arr = str.split('/');
-    str = arr[arr.length - 1];
-
-    return str;
-};
-
-export default getRealName;
--- scripts/trans-big-hump-name.js
+++
@@ -1,3 +0,0 @@
-const transBigHumpName = (str) => (str.substr(0, 1).toUpperCase() + str.substr(1)).replace(/-(\w)/g, ($0, $1) => $1.toUpperCase());
-
-export default transBigHumpName;
